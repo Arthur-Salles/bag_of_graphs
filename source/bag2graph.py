e79d7fa67eaa851133a363828ba9cdf78bd79431
Arthur Salles <a166003@dac.unicamp.br>
2024-08-27 20:56:31 -0300

    added multivariate compatibility to apply_bow method

--- source/bag2graph.py
+++ source/bag2graph.py
@@ -15,6 +15,7 @@ class Bag2Graph():
         window_size, 
         word_size,
         n_bins,
+        channel_size,
     ):
         
         self.bow = pyts.bag_of_words.BagOfWords(window_size, word_size, n_bins)
@@ -22,7 +23,7 @@ class Bag2Graph():
         self.dict_words = np.apply_along_axis("".join, axis=1,arr=list(itertools.product(*([[i for i in string.ascii_letters[:n_bins]]] * word_size))))
 
         print(self.dict_words, len(self.dict_words))
-
+        self.channel_size = channel_size
     
     def get_filtered_words(self):
         return self.selected_words
@@ -39,6 +40,10 @@ class Bag2Graph():
         return list(X_BoP.values())
             
 
+    def __select_agg(self, aggregation_type):
+        return np.sum
+
+
     def __filter_words(self, x_bop, y, K=10):
         f_chi2, p_values = chi2(x_bop, y)
         f_chi2_sorted = np.argsort(f_chi2)
@@ -48,38 +53,82 @@ class Bag2Graph():
         self.selected_words = self.dict_words[selected_fchi2]
         self.filtered_bop = x_bop[:, selected_fchi2]
         
-        print(f'Filtered {K} top words :', self.selected_words)
+        print(f'Filtered {K} top words :', self.selected_words,'\n')
 
 
-    def apply_bow_uni(self, x, y, inx=0):
-        # fix this later , check the TODO 
-        x = x[:, inx] # TODO : add compatibility for other channels and variable length 
-        X = check_array(x)
-        X_bow = self.bow.fit_transform(X)
-        x_bop = np.array([ self.__get_count_from_bow(xb) for xb in X_bow])
 
-        self.__filter_words(x_bop, y)
+    # def apply_bow_uni(self, x, y, inx=0):
+    #     x = x[:, inx] # TODO : add compatibility for variable length 
+    #     X = check_array(x)
+    #     X_bow = self.bow.fit_transform(X)
+    #     x_bop = np.array([ self.__get_count_from_bow(xb) for xb in X_bow])
 
+    #     self.__filter_words(x_bop, y)
+
+    #     filtered_xbow = []
+    #     for inx, bow in enumerate(X_bow):
+    #         splited_bow = np.array(bow.split(' '))
+    #         # print(splited_bow)
+    #         mask = np.isin(splited_bow, self.selected_words)
+    #         filtered_bow = splited_bow[mask]
+    #         filtered_xbow.append(filtered_bow)
+
+    #         print(filtered_xbow)
+        
+    #     self.filtered_xbow = filtered_xbow
+
+    #     return filtered_xbow
+
+
+
+    def apply_bow(self, x, y, aggregation_type='sum'):
+
+        t_xbop = []
+        t_Xbow = []
         filtered_xbow = []
-        for inx, bow in enumerate(X_bow):
-            splited_bow = np.array(bow.split(' '))
-            mask = np.isin(splited_bow, self.selected_words)
-            filtered_bow = splited_bow[mask]
-            filtered_xbow.append(filtered_bow)
 
-        self.filtered_xbow = filtered_xbow
+        # TODO : select aggregation method for multivariable here or maybe in another object lol
+        agg = self.__select_agg(aggregation_type)
+        
+        for ch in range(self.channel_size):
+            
+            X = x[:, ch]
+            X = check_array(X)
+            X_bow = self.bow.fit_transform(X)
+            # print(X_bow.shape)
+            x_bop = np.array([ self.__get_count_from_bow(xb) for xb in X_bow])
+            t_xbop.append(x_bop)
+            t_Xbow.append(X_bow)
+        
+        t_xbop = np.array(t_xbop)
+        t_xbop = agg(t_xbop, axis=0)
 
-        # return filtered_xbow
+        self.__filter_words(t_xbop, y)        
 
-    def get_cooc_matrix(self, inx, include_diagonal=False):
+        t_Xbow = np.array(t_Xbow).T
+        print(t_Xbow.shape) # CANAL, 40 vetores de palavras, 
+
+        # # precisa filtrar todas palavras no bow por canal a agregacao vai ser dar no grafo 
+        for ch, word_per_ch in enumerate(t_Xbow):
+            split_w = [np.array(w.split(' ')) for w in word_per_ch]
+            f_w = [p[np.isin(p, self.selected_words)] for p in split_w]
+
+            filtered_xbow.append(f_w)
+
+        filtered_xbow = np.array(filtered_xbow, dtype='object')
+        print('shape',filtered_xbow.shape)
+
+
+
+        
+
+    def __get_inx_cooc_matrix(self, inx, include_diagonal=False):
 
         # # get words to build the fera graph
         f_bow = self.filtered_xbow[inx]
 
         # # making the maximum possible matrix to be able to sum later
         max_words = np.unique((self.selected_words.flatten()))
-        indx_words = dict(zip(max_words, range(len(max_words))))
-        r_indx_words = dict(zip(range(len(max_words)), max_words))
         n = len(max_words) ## the adj will be ->  n x n matrix
 
         # # creating the adjancey words : this could be usefull in the future to experiment K-connections        
@@ -87,21 +136,21 @@ class Bag2Graph():
         neighbors[:, 0] = f_bow[:-1]
         neighbors[:, 1] = f_bow[1:]
 
-        ## vectorized + hash 
+        ###  vectorized + hash 
         def __transform_word(a, b): 
             n1, n2 = hash(a) % n, hash(b) % n 
-            # n1, n2 = indx_words[a] % n, indx_words[b] % n 
             return n1, n2
 
         v_func = np.vectorize(__transform_word)
 
-        adj_m = np.zeros((n,n))
+        adj_m = np.zeros((n, n))
 
         edges = v_func(neighbors[:, 0], neighbors[:, 1])
         print(edges)
         edges_list = np.stack((edges[0], edges[1]), axis=-1) 
         pairs, counts = np.unique(edges_list, axis = 0, return_counts=True)
-        adj_m[pairs[:, 0], pairs[:, 1]] = counts 
+
+        adj_m[pairs[:, 0], pairs[:, 1]] = counts  # CAN THIS BE DONE WITHOUT CREATING AN ADJ MATRIX ?????
 
         if not include_diagonal: 
             np.fill_diagonal(adj_m, 0)
@@ -110,5 +159,11 @@ class Bag2Graph():
         normalized_adj = adj_m / row_sum[:, np.newaxis]
         normalized_adj = np.nan_to_num(normalized_adj, nan = 0)
 
-        return adj_m, neighbors, edges_list
+        return normalized_adj, neighbors, edges_list
 
+    def get_cooc_matrix(self, x, aggregation_type='sum', include_diagonal=False):
+
+        for ch in range(self.channel_size):
+            
+            X = x[:, ch]
+            n_adj, _, edges_list = self.__get_inx_cooc_matrix(ch, False)
